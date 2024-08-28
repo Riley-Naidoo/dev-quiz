98487a2e1c0171d6026673fe1f1f4a84d28ac456
Riley Naidoo <119883343+Riley-Naidoo@users.noreply.github.com>
2024-08-28 14:06:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const hamMenu = document.querySelector('.ham-menu');
+    const offScreenMenu = document.querySelector('.off-screen-menu');
+  
+    hamMenu.addEventListener('click',()=>{
+      hamMenu.classList.toggle('active');
+      offScreenMenu.classList.toggle('active');
+    })
+  
+    dropdownToggle.addEventListener('click', function () {
+      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    });
+  
+    // Close the dropdown if clicked outside
+    document.addEventListener('click', function (e) {
+      if (!dropdownToggle.contains(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.style.display = 'none';
+      }
+    });
+  });
+
